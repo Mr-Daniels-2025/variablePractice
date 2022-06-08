d8d17c029ec5b302b07ff6bee40cc92c7d619ba3
daniel <mr.daniels.2025@gmail.com>
2022-06-07 18:42:18 -0600

    Add variables

--- variables.js
+++ variables.js
@@ -1,3 +1,15 @@
-var email = "helloworld@gmail.com"
-var startingAge = 37
-var depositPaid = true
+var email = "google@gmail.com"
+var startingAge = 47
+var depositPaid = false
+var firstName = "Dan"
+var lastName = "Daniels"
+
+//reassign string
+firstName = "Daniel"
+lastName = "D"
+
+console.log(email)
+console.log(startingAge)
+console.log(depositPaid)
+console.log(firstName)
+console.log(lastName)
